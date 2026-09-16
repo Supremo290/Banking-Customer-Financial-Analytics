1d140388e453ae87323bf8027cfd2f52acdd6034
Robbie Fuggan <148744488+Supremo290@users.noreply.github.com>
2026-09-17 02:14:20 +0800

    Add raw client table definition
    Added a new table for raw client data with relevant fields.

--- sql/01_database_schema.sql
+++ sql/01_database_schema.sql
@@ -4,3 +4,10 @@
 
 CREATE SCHEMA IF NOT EXISTS raw;
 CREATE SCHEMA IF NOT EXISTS core;
+
+-- Raw customer source table
+CREATE TABLE IF NOT EXISTS raw.client (
+    client_id INTEGER,
+    birth_number INTEGER,
+    district_id INTEGER
+);
